79c7ca720d2bb7f39e4464508c1553c7032a3c77
melisbekovkutman <melisbekovkutman@gmail.com>
2022-05-31 19:14:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import q from '../src/img/04.05.jpg'
+import w from '../src/img/nophotoshop29-800x532.jpg'
 function App() {
+  let [img, image] = useState(q)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <div>
+    <h1>{hello}</h1>
+    
+    <button onClick={()=>setBir('hello')}>+</button>
+    <button onClick={()=>setBir('text')}>+</button>
+    </div> */}
+  
+  <div>
+  <img src={q} width={100} height={60}/>
+  <img src={w} width={100} height={60}/>
+  
+  </div>
+  <div><img src={img}/></div>
+    <div className='btn'>
+    <button onClick={()=>image(q)}>1foto</button>
+    <button onClick={()=>image(w)}>2foto</button>
+    
+    </div>
+    <div className='foto'>
+    <img onClick={()=>image(q)} src={q} width={40}/>
+    <img onClick={()=>image(w)} src={w} width={40}/>
+    </div>
     </div>
   );
 }
---
+++ src/components/block.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+function block(){
+    
+}
+const block = () => {
+
+  return (
+    <div>b</div>
+  )
+}
+
+export default block
